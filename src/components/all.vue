82a29dc0a329ca25db4e533551be3734ce1d7a79
Hanchi Ahmed <hanchi.ahmed@yahoo.fr>
2020-03-30 17:08:07 +0100

    s commit

--- src/components/all.vue
+++ src/components/all.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <p>hi</p>
+        
+        <p v-for="(stat , index) in Stats" v-bind:key="stat.countryInfo._id" v-bind:index="index" >
+            <img v-bind:src="stat.countryInfo.flag"/>  
+            countrie : {{stat.country}} -- cases : {{stat.cases}}
+        </p>
     </div>
 </template>
 
@@ -23,6 +28,7 @@ export default {
         await axios.get('https://corona.lmao.ninja/countries?sort=country')
         .then((res)=>{
             this.Stats = res.data
+            this.Stats = this.Stats.sort((a, b) =>  b.country - a.country  );
 
         })
         .catch((err) =>{
